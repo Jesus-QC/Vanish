38e31ff1fe77fe5257e723dda400601a86067b5d
jesusqc <jesusquirocampos@gmail.com>
2024-01-25 18:15:51 +0100

    Ver bump

--- Vanish/EntryPoint.cs
+++ Vanish/EntryPoint.cs
@@ -10,7 +10,7 @@ namespace Vanish;
 
 public class EntryPoint
 {
-    public const string Version = "1.0.0.6";
+    public const string Version = "1.0.0.7";
     
     [PluginAPI.Core.Attributes.PluginConfig] public static PluginConfig Config;
     
